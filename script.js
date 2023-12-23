e2426f16ff1246a76736c2ba4ce4cfd90e0a3036
Hundredz <144457401+Hundredzz@users.noreply.github.com>
2023-12-23 23:44:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,82 +1,39 @@
-const sliderValue = document.getElementById("slider-value");
-const sliderInput = document.getElementById("slider");
-const passwordBox = document.getElementById("Password");
-const upperCheck = document.getElementById("Uppercase");
-const lowerCheck = document.getElementById("Lowercase");
-const numberCheck = document.getElementById("Number");
-const symbolCheck = document.getElementById("Symbol");
-
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "@#$%^&*()_+~{[}]|<>/-=";
-
-function createPassword(){
-    let password = "";
-    let allChar = "";
-    if(upperCheck.checked === true){
-        password += upperCase[Math.floor(Math.random() * upperCase.length)];
-        allChar += upperCase;
-    }
-    if(lowerCheck.checked === true){
-        password += lowerCase[Math.floor(Math.random() *lowerCase.length)];
-        allChar += lowerCase;
-    }
-    if(numberCheck.checked === true){
-        password += number[Math.floor(Math.random() * number.length)];
-        allChar += number;
-    }
-    if(symbolCheck.checked === true){
-        password += symbol[Math.floor(Math.random() * symbol.length)];
-        allChar += symbol;
-    }
-
-    while(sliderInput.value > password.length){
-        password += allChar[Math.floor(Math.random() * allChar.length)];
-    }
-    
-    if (!upperCheck.checked && !lowerCheck.checked && !numberCheck.checked && !symbolCheck.checked) {
-        passwordBox.value = "";
-    } else {
-        passwordBox.value = password;
-    }
-
-}
-
-function copyPassword(){
-    passwordBox.select();
-    document.execCommand("copy");
-}
-
-sliderInput.oninput = (() => {
-    let value = sliderInput.value;
-    sliderValue.textContent = value;
+const wac = document.getElementById("asi");
+const wac2 = document.getElementById("asi_2");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
 });
 
-const container = document.querySelectorAll(".slider-bar");
-
-for (let i = 0; i < container.length; i++) {
-    const slider = container[i].querySelector(".slider");
-    const thumb = container[i].querySelector(".slider-thumb");
-    const progress = container[i].querySelector(".progress");
-
-    function customSlider() {
-        const maxVal = parseFloat(slider.getAttribute("max"));
-        const sliderValue = parseFloat(slider.value); 
-
-        let val = 0; 
-
-        if (sliderValue < 13) {
-            val = ((sliderValue - 5.85) / (maxVal - 6)) * 100 + "%"; 
-        } else {
-            val = ((sliderValue - 6) / (maxVal - 5.7)) * 100 + "%"; 
-        }
-
-        progress.style.width = val;
-        thumb.style.left = val;
+const hiddenElements = document.querySelectorAll('.hidden');
+function typeWriter(element, text, index, speed) {
+    if (index < text.length) {
+        element.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(() => typeWriter(element, text, index, speed), speed);
     }
-
-    customSlider();
-
-    slider.addEventListener("input", customSlider); 
 }
+
+setTimeout(function () {
+    typeWriter(wac, "Asitara Phumdokmai", 0, 100);
+}, 500);
+
+setTimeout(function () {
+    typeWriter(wac2, "Asitara Phumdokmai", 0, 100);
+}, 500);
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+hiddenElements.forEach((el) => observer.observe(el));
